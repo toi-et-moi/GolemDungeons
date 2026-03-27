904deb4f9b0661bf80ef2cf9a05287cc5bf55534
toi-et-moi <3423886226@qq.com>
2026-03-27 16:48:00 +0800

    简易铜刻阵营配置

---
+++ src/main/java/dev/xkmc/golemdungeons/compat/jerotes_village_golems/package-info.java
@@ -0,0 +1,8 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+
+package dev.xkmc.golemdungeons.compat.jerotes_village_golems;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
